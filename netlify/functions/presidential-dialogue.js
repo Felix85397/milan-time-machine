92ac5e2b111f1d477727a30da304a113a94e1fd6
Felix85397 <felice.raimondo85@gmail.com>
2026-01-25 14:38:52 +0100

    Update presidential-dialogue.js

--- netlify/functions/presidential-dialogue.js
+++ netlify/functions/presidential-dialogue.js
@@ -74,7 +74,7 @@ Continua alternando per 6 battute totali. Usa un linguaggio appropriato all'epoc
     console.log('Generando dialogo presidenziale...');
 
     const controller = new AbortController();
-    const timeoutId = setTimeout(() => controller.abort(), 25000);
+    const timeoutId = setTimeout(() => controller.abort(), 45000);
 
     try {
       const response = await fetch('https://api.anthropic.com/v1/messages', {
